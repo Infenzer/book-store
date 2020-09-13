6b609ca0eebe4c3b0d3635c39d9e5c3c3bc6229d
Vitaliy <vitalik201444@gmail.com>
2020-09-13 20:49:42 +0300

    slider indicators

--- frontend/src/app/components/slider/slider.component.ts
+++ frontend/src/app/components/slider/slider.component.ts
@@ -1,5 +1,10 @@
 import {  AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
+interface IIndicator {
+  id: number
+  active: boolean
+}
+
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
@@ -8,6 +13,7 @@ import {  AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angul
 export class SliderComponent implements OnInit, AfterViewInit {
   counter = 1
   slideWidth = 0
+  indicators: IIndicator[] = []
 
   @ViewChild('sliderContent') sliderContent: ElementRef<HTMLDivElement>
   @ViewChild('slide') slide: ElementRef<HTMLDivElement>
@@ -20,6 +26,7 @@ export class SliderComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     const width = window.getComputedStyle(this.slide.nativeElement).width
     this.setSlideWidth(width)
+    this.initIndicators(this.sliderContent.nativeElement.children)
 
     this.sliderContent.nativeElement.addEventListener('transitionend', () => {
       const sliderContent = this.sliderContent.nativeElement
@@ -56,12 +63,45 @@ export class SliderComponent implements OnInit, AfterViewInit {
     }
 
     sliderContent.style.left = leftPosition + 'px'
-    this.sliderContent.nativeElement.style.transition = ''
+    sliderContent.style.transition = ''
+
+    this.setIndicators()
   }
 
   setSlideWidth(width: string) {
     let numberWidth = parseInt(width.match(/\d+/)[0])
     this.slideWidth = numberWidth
+  }
+
+  setIndicators() {
+    let iCounter = this.counter
+
+    if (this.counter > this.indicators.length) {
+      iCounter = 1
+    } else if (this.counter < 1) {
+      iCounter = this.indicators.length
+    }
+
+    this.indicators.forEach(indicator => {
+      indicator.active = indicator.id === iCounter ? true : false
+    })
+  }
+
+  initIndicators(children: HTMLCollection) {
+    for (let i = 1; i <= children.length - 2; i++) {
+      this.indicators.push({
+        id: i,
+        active: i === this.counter ? true : false
+      })
+    }
+  }
+
+  onIndicatorClick(id: number) {
+    this.sliderContent.nativeElement.style.left = -(this.slideWidth * id) + 'px'
+    this.sliderContent.nativeElement.style.transition = ''
+    this.counter = id
+    
+    this.setIndicators()
   }
 
 }
